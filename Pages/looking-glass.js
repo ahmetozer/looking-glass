053db55f6e026db9da056a6dea01133d1fd787b2
Ahmet <10574489+AhmetOZER@users.noreply.github.com>
2020-07-10 19:03:46 +0300

    Json minify

--- Pages/looking-glass.js
+++
@@ -1,9 +0,0 @@
----
-layout: none
-title:  ""
-permalink: "assets/js/looking-glass.js"
----
-{% include javascript/regex.js %}
-{% include javascript/default_config.js %}
-{% include javascript/server_select_modal.js %}
-{% include javascript/main.js %}
---
+++ assets/javascript/default_config.js
@@ -0,0 +1,1 @@
+var default_server_config = JSON.parse('{"ServerConfig":{"whois":"disabled","nslookup":"disabled","ping":"disabled","icmp":"disabled","tracert":"disabled","webcontrol":"disabled","tcp":"disabled","IPv6":"disabled","IPv4":"disabled","curl":"disabled"}}');
