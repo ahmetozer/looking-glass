cdcd2e2e28f37bca589551a7e8eb47e54cd3c2ba
Ahmet <10574489+AhmetOZER@users.noreply.github.com>
2020-07-25 16:40:47 +0300

    For javascript debugging

---
+++ assets/javascript/lg.js
@@ -0,0 +1,8 @@
+---
+layout: none
+permalink: /assets/javascript/lg.js
+---
+{% include_relative default_config.js %}
+{% include_relative main.js %}
+{% include_relative regex.js %}
+{% include_relative server_select_modal.js %}
